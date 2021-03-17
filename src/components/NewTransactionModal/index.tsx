e8c03d7e27f3004d649744087b94cd8c46fc27a9
igorWebMASTER <igor_1917@hotmail.com>
2021-03-17 20:33:53 -0300

    refactor - creating hook useTransactions

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,4 @@
-import {FormEvent, useState, useContext} from 'react';
+import {FormEvent, useState} from 'react';
 import Modal from 'react-modal';
 import { Container,TransactionTypeContainer, RadioBox } from './styles';
 
@@ -6,7 +6,7 @@ import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 
-import { TransactionsContext } from '../../TransactionsContext';
+import { useTransactions } from '../../hooks/useTransactions';
 
 
 interface NewTransactionModalProps{
@@ -15,7 +15,7 @@ interface NewTransactionModalProps{
 }
 
 export function NewTransactionModal( {isOpen, onRequestClose}:NewTransactionModalProps ){
-  const { createTransaction } = useContext(TransactionsContext);
+  const { createTransaction } = useTransactions();
   
   const [title,setTitle] = useState('');
   const [amount,setAmount] = useState(0);
--- src/hooks/useTransactions.tsx
+++ src/hooks/useTransactions.tsx
@@ -1,5 +1,5 @@
-import { createContext, useState, useEffect, ReactNode} from 'react';
-import { api } from './services/api';
+import { createContext, useState, useEffect, ReactNode, useContext} from 'react';
+import { api } from '../services/api';
 
 interface Transaction{
   id: number;
@@ -21,7 +21,7 @@ interface TransactiosContextData{
   createTransaction: (transaction: TransactionInput) => Promise<void>;
 }
 
-export const TransactionsContext = createContext<TransactiosContextData>(
+const TransactionsContext = createContext<TransactiosContextData>(
   {} as TransactiosContextData
 );
 
@@ -50,4 +50,10 @@ export function TransactionsProvider({children}: TransactionsProviderProps){
   return <TransactionsContext.Provider value={{ transactions, createTransaction }}>
             {children}
         </TransactionsContext.Provider>;
+}
+
+export function useTransactions(){
+  const context = useContext(TransactionsContext);
+
+  return context;
 }
